104f4b78c61242a5e14d5110fdc98d30d4449e20
akshayaki <57026744+akshayaki@users.noreply.github.com>
2019-10-26 11:47:29 +0530

    added function
    added function to calculate the gcd

--- HactoberFest_Programs/gcd.c
+++ HactoberFest_Programs/gcd.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
-int gcd(int a,int b){
-	//Write code here
-}
+int gcd(int a,int b){while (a != b)
+    {
+        if (a > b)
+        {
+            return gcd(a - b, b);
+        }
+        else
+        {
+            return gcd(a, b - a);
+        }
+    }
+    return a;
 int main(){
 	int num1,num2,res=0;
 	printf("\nEnter 2 numbers : ");
