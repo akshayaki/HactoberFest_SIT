62d365d8c57a98ef2b0d6b4b1fb229ba825b74e8
akshayaki <57026744+akshayaki@users.noreply.github.com>
2019-10-28 15:58:23 +0530

    changed prime function
    changed full function

--- HactoberFest_Programs/prime.c
+++ HactoberFest_Programs/prime.c
@@ -1,31 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-	int i,num,flag=0,count=0,start,end;
-	printf("\nEnter starting number: ");
-	scanf("%d",&start);
-	printf("\nEnter ending number: ");
-	scanf("%d",&end);
-	for(num=start;num<end;num++){
-		if (num==0||num==1){
-			flag=1;
-			continue;
-		}
-		for(i=2;i<num/2;i++){
-			if(num%i==0){
-				flag=1;
-				break;
-			}
-		}
-		if(flag==0){
-			count++;
-		}
-		else{
-			flag=0;
-		}
-		
-	}
-	printf("\nTotal number of primes is :%d\n",count);
-
-	return 0;
+int main()
+{
+    int n, i, flag = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    for(i = 2; i <= n/2; ++i)
+    {
+        // condition for nonprime number
+        if(n%i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (n == 1) 
+    {
+      printf("1 is neither a prime nor a composite number.");
+    }
+    else 
+    {
+        if (flag == 0)
+          printf("%d is a prime number.", n);
+        else
+          printf("%d is not a prime number.", n);
+    }
+    
+    return 0;
 }
